a5a4c3b8dade52f880af018cc676c038c3dbfd96
Silvano Amaral <silvano.amaral9@gmail.com>
2018-09-05 17:42:39 -0300

    Funcao Temperatura

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+let convert = {};
+
 //is a function for converting Celsius temperatures to Fahrenheit
-cToF = function(celsius) {
+convert.cToF = function(celsius) {
     if(!Number.isInteger(celsius)) return undefined;
     return celsius * 9 / 5 + 32;
 }
 //is the reverse function. It converts Fahrenheit to Celsius
-fToC = function(fahrenheit) {
+convert.fToC = function(fahrenheit) {
     if(!Number.isInteger(fahrenheit)) return undefined;
     return (fahrenheit - 32) * 5 / 9;
 }
+
+module.exports = convert;
